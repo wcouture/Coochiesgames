f5742fc550033b5ffabf374a3460d4566d3b2869
gnarlsVT <58340991+gnarlsVT@users.noreply.github.com>
2020-09-27 23:29:53 -0400

    1

---
+++ top.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Coochie's Games</title>
+        <meta name="author" content="Will Couture, and Owen Cooledge">
+        <meta name="description" content="Coochie's games is a website to showcase games created by Will Couture">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/custom.css?version=<?php print time(); ?>" type="text/css">
+      </head>
